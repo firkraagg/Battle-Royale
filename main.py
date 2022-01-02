236cda510c9bf9c302e02a436ed910d80f4b0f5c
firkraagg <93527124+firkraagg@users.noreply.github.com>
2022-01-02 22:06:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import utilities
 import level
 import scene
 import globals
-import config
+import button
+import inputstream
 from config import *
 
 pygame.init()
@@ -12,56 +13,60 @@ window = pygame.display.set_mode((720, 480))
 pygame.display.set_caption('Battle Royale')
 clock = pygame.time.Clock()
 font1 = pygame.font.SysFont('franklingothicmedium', 25)
+gui_font = pygame.font.SysFont('franklingothicmedium', 24)
+font = pygame.font.SysFont('franklingothicmedium', 15)
 
 game_state = 'playing'
 
 entities = []
 
-second_surface = pygame.Surface([720, 400])
-second_surface.fill((BLACK))
-door_text = "press 1 to go to hall"
-text0 = "press E to talk"
-text1 = "Hello, I am Spiculus. They brought you here last night. I don't know "
-text2 = "what crime you committed, but you got into the gladiatorial arena."
-text3 = "You have to defeat three chosen gladiators and then you will be free. "
-
-
-
-gui_font = pygame.font.SysFont('franklingothicmedium', 24)
-font = pygame.font.SysFont('franklingothicmedium', 15)
-dialogue_surface0 = font.render(text0, False, '#FFFFFF')
-door_surface = font.render(door_text, False, '#FFFFFF')
-dialogue_surface1 = gui_font.render(text1, False, '#FFFFFF')
-dialogue_surface2 = gui_font.render(text2, False, '#FFFFFF')
-dialogue_surface3 = gui_font.render(text3, False, '#FFFFFF')
-
-p_background = pygame.image.load('images/Prison/prison.png')
+p_background = pygame.image.load('images/Backgrounds/prison_bg.png')
 p_background = pygame.transform.scale(p_background, (GAME_WIDTH, GAME_HEIGHT))
-
-hall_bg = pygame.image.load("images/Hall/hall_bg.png")
-hall_bg = pygame.transform.scale(hall_bg, (GAME_WIDTH, GAME_HEIGHT))
-
-shop_text = "Press 3 to go to the shop"
-shop_surface = font.render(shop_text, False, '#FFFFFF')
 
 coin_image = pygame.image.load('images/Coins/coin.png')
 coin = utilities.makeCoin(90, 342)
-coin1 = utilities.makeCoin(500, 342)
+coin1 = utilities.makeCoin(615, 342)
+coin2 = utilities.makeCoin(200, 342)
 entities.append(coin)
 
-npc = utilities.makeNpc(150, 226)
+npc1 = utilities.makeNpc(350, 226)
+shopkeeper = utilities.makeShopKeeper(250,242)
 
-player = utilities.makePlayer(530, 261)
+sword = utilities.makeSword(255, 280)
+armor = utilities.makeArmor(310, 274)
+potion = utilities.makePotion(445, 293)
+
+
 background = utilities.makeBackground(p_background)
+player = utilities.makePlayer(450, 261)
 player.camera = engine.Camera(0, 0, 920, 525)
 player.score = engine.Score()
+player.potions = engine.Potions()
+player.damage = engine.Damage()
+player.health = engine.Health()
+player.maxHealth = engine.MaxHealth()
+player.input = engine.Input(pygame.K_a, pygame.K_d)
+player.intention = engine.Intention()
+score = 0
 
+player1 = utilities.makePlayer1(100, 363)
+player1.camera = engine.Camera(0, 0, 920, 525)
+player1.score = player.score
+player1.potions = player.potions
+player1.damage = player.damage
+player1.health = player.health
+player1.maxHealth = engine.MaxHealth()
 
-door1 = pygame.Rect(646, 405, 5, 50)
+enemy = utilities.makeEnemy(460, 360, 5, 1)
+enemy.camera = engine.Camera(0, 0, 920, 525)
+enemy.score = player.score
+enemy.potions = player.potions
+enemy.damage = player.damage
+enemy.enemyHealth = engine.EnemyHealth()
 
+door1 = pygame.Rect(46, 405, 5, 50)
 
 globals.levels[1] = level.Level(
-
     platforms=[
         pygame.Rect(52, 350, 610, 5),
 
@@ -71,8 +76,7 @@ globals.levels[1] = level.Level(
 
     ],
     entities=[
-        npc, player, coin
-
+        npc1, player, coin
     ],
     doors=[
         door1
@@ -80,14 +84,12 @@ globals.levels[1] = level.Level(
 
 )
 globals.levels[2] = level.Level(
-
     platforms=[
         pygame.Rect(52, 350, 610, 5),
 
         pygame.Rect(25, 405, 5, 50),
 
         pygame.Rect(656, 405, 5, 50),
-
     ],
     entities=[
         coin1, player
@@ -95,11 +97,9 @@ globals.levels[2] = level.Level(
     doors=[
         door1
     ]
-
 )
 
 globals.levels[3] = level.Level(
-
     platforms=[
         pygame.Rect(52, 350, 610, 5),
 
@@ -108,16 +108,14 @@ globals.levels[3] = level.Level(
         pygame.Rect(656, 405, 5, 50)
     ],
     entities=[
-        coin1, player
+        coin2, shopkeeper, sword, potion, armor, player
     ],
     doors=[
         pygame.Rect(330, 405, 5, 50),
     ]
-
 )
 
 globals.levels[4] = level.Level(
-
     platforms=[
         pygame.Rect(52, 350, 610, 5),
 
@@ -126,34 +124,34 @@ globals.levels[4] = level.Level(
         pygame.Rect(656, 405, 5, 50)
     ],
     entities=[
-        player
+        player1, enemy
     ],
     doors=[
         pygame.Rect(330, 405, 5, 50),
     ]
-
 )
 
 
 globals.world = globals.levels[1]
 
-
 sceneManager = scene.SceneManager()
 mainMenu = scene.MainMenuScene()
 sceneManager.push(mainMenu)
 
+inputStream = inputstream.InputStream()
 cameraSys = engine.CameraSystem()
+cameraSys1 = engine.CameraSystem1()
 running = True
 while running:
+    inputStream.processInput()
 
-    sceneManager.input()
-    sceneManager.update()
+    sceneManager.input(inputStream)
+    sceneManager.update(inputStream)
     sceneManager.draw(window)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
 
     new_player_x = player.position.rect.x
 
@@ -169,9 +167,7 @@ while running:
     if not keys[pygame.K_a] and not keys[pygame.K_d]:
         player.state = 'standing'
 
-
     if game_state == 'playing':
-
 
         for entity in globals.world.entities:
             entity.animations.animationList[entity.state].update()
@@ -180,10 +176,6 @@ while running:
 
         new_player_rect = pygame.Rect(int(new_player_x), 400, 72, 72)
         x_collision = False
-        # if config.levels[1]:
-        #     utilities.makeBackground(p_background)
-        # if config.levels[2]:
-        #     utilities.makeBackground(hall_bg)
 
         for platform in globals.world.platforms:
             if platform.colliderect(new_player_rect):
@@ -192,37 +184,46 @@ while running:
 
         for door in globals.world.doors:
             if door.colliderect(new_player_rect):
-                window.blit(door_surface, (530, 225))
-
-
-
+                window.blit(utilities.door_surface, (70, 225))
 
         if x_collision == False:
             player.position.rect.x = new_player_x
-
-        score = 0
+        sword_text = "Sword"
+        sword_surface = font.render(sword_text, False, '#FFFFFF')
         player_rect = pygame.Rect(int(player.position.rect.x), 300, player.position.rect.width, player.position.rect.height)
         for entity in globals.world.entities:
-            if entity.type == 'collectable':
-                if entity.position.rect.colliderect(player_rect):
-                    globals.world.entities.remove(entity)
-                    utilities.draw_coinText(window, str(score), 80, 52)
-                    player.score.score += 15
-
+             if entity.type == 'buyable':
+                 if entity.position.rect.colliderect(player_rect):
+                     if event.type == pygame.KEYUP:
+                         if event.key == pygame.K_e:
+                             if entity == sword:
+                                 if player.score.score >= 20:
+                                     player.score.score -= 20
+                                     player.damage.damage += 3
+                                     globals.world.entities.remove(entity)
+                             if entity == potion:
+                                 if player.score.score >= 10:
+                                     player.score.score -= 10
+                                     player.potions.potions += 1
+                                     globals.world.entities.remove(entity)
+                             if entity == armor:
+                                 if player.score.score >= 20:
+                                     player.score.score -= 20
+                                     player1.maxHealth.maxHealth += 7
+                                     player.maxHealth.maxHealth += 7
+                                     player1.health.health += 7
+                                     globals.world.entities.remove(entity)
         for entity in globals.world.entities:
             if entity.type == 'friendly':
                 if entity.position.rect.colliderect(player_rect):
-                    window.blit(dialogue_surface0, (175, 240))
+                    window.blit(utilities.dialogue_surface0, (370, 240))
                     if event.type == pygame.KEYUP:
                         if event.key == pygame.K_e:
-                            window.blit(second_surface, (0, 370)),
-                            window.blit(dialogue_surface1, (0, 380)),
-                            window.blit(dialogue_surface2, (0, 410)),
-                            window.blit(dialogue_surface3, (0, 440))
+                            window.blit(utilities.second_surface, (0, 370)),
+                            window.blit(utilities.dialogue_surface1, (0, 380)),
+                            window.blit(utilities.dialogue_surface2, (0, 410)),
+                            window.blit(utilities.dialogue_surface3, (0, 440))
 
-        # draw_text(str(coin_number), 80, 52)
-        # cameraSys.update(window, world)
-        # pygame.display.flip()
         clock.tick(60)
 
 pygame.quit()
--- utilities.py
+++ utilities.py
@@ -3,13 +3,11 @@ import engine
 from config import *
 
 pygame.init()
-
-
 coin_im = pygame.image.load('images/Coins/coin.png')
 
 def makeCoin(x, y):
     entity = engine.Entity()
-    entity.position = engine.Position(x,y,1,1)
+    entity.position = engine.Position(x,y,18,20)
     entityAnimation = engine.Animation([coin_im])
     entity.animations.add('standing', entityAnimation)
     entity.type = 'collectable'
@@ -38,6 +36,81 @@ def makeNpc(x, y):
     entity.type = 'friendly'
     return entity
 
+shopkeeper_image = pygame.image.load("images/Npc/shopkeeper.png")
+shopkeeper_image = pygame.transform.scale(shopkeeper_image, (135, 135))
+shopkeeper_image1 = pygame.image.load("images/Npc/shopkeeper1.png")
+shopkeeper_image1 = pygame.transform.scale(shopkeeper_image1, (135, 135))
+shopkeeper_image2 = pygame.image.load("images/Npc/shopkeeper3.png")
+shopkeeper_image2 = pygame.transform.scale(shopkeeper_image2, (135, 135))
+shopkeeper_image3 = pygame.image.load("images/Npc/shopkeeper4.png")
+shopkeeper_image3 = pygame.transform.scale(shopkeeper_image3, (135, 135))
+def makeShopKeeper(x, y):
+    entity = engine.Entity()
+    entity.position = engine.Position(x, y, 80, 250)
+    entityAnimation = engine.Animation([shopkeeper_image, shopkeeper_image1, shopkeeper_image3, shopkeeper_image1])
+    entity.animations.add('standing', entityAnimation)
+    entity.type = 'friendly'
+    return entity
+
+sword_image = pygame.image.load("images/ShopItems/sword.png")
+sword_image = pygame.transform.scale(sword_image, (60, 60))
+def makeSword(x, y):
+    entity = engine.Entity()
+    entity.position = engine.Position(x, y, 40, 40)
+    entityAnimation = engine.Animation([sword_image])
+    entity.animations.add('standing', entityAnimation)
+    entity.type = 'buyable'
+    entity.type1 = 'sword'
+    return entity
+
+potion_image = pygame.image.load("images/ShopItems/potion.png")
+potion_image = pygame.transform.scale(potion_image, (30, 30))
+potion_image1 = pygame.image.load("images/ShopItems/potion1.png")
+potion_image1 = pygame.transform.scale(potion_image1, (30, 30))
+potion_image2 = pygame.image.load("images/ShopItems/potion2.png")
+potion_image2 = pygame.transform.scale(potion_image2, (30, 30))
+def makePotion(x, y):
+    entity = engine.Entity()
+    entity.rect = pygame.rect.Rect(10, 10, 60, 40)
+    entity.position = engine.Position(x, y, 60, 40)
+    entityAnimation = engine.Animation([potion_image, potion_image1, potion_image2])
+    entity.animations.add('standing', entityAnimation)
+    entity.type = 'buyable'
+    entity.type1 = 'potion'
+    return entity
+
+armor_image = pygame.image.load("images/ShopItems/armor.png")
+armor_image = pygame.transform.scale(armor_image, (70, 70))
+def makeArmor(x, y):
+    entity = engine.Entity()
+    entity.position = engine.Position(x,y,40,40)
+    entityAnimation = engine.Animation([armor_image])
+    entity.animations.add('standing', entityAnimation)
+    entity.type = 'buyable'
+    entity.type1 = 'armor'
+    return entity
+
+# class HealthBar:
+#     def __init__(self, x, y, hp, max_hp):
+#         self.x = x
+#         self.y = y
+#         self.hp = hp
+#         self.max_hp = max_hp
+#     def draw(self, window, hp):
+#         self.hp = hp
+#         ratio = self.hp / self.max_hp
+#         pygame.draw.rect(window, RED, (self.x, self.y, 150, 20))
+#         pygame.draw.rect(window, GREEN, (self.x, self.y, 150 * ratio, 20))
+#
+# class Hp:
+#     def __init__(self, hp):
+#         self.hp = hp
+#
+# class MaxHp:
+#     def __init__(self, max_hp):
+#         self.max_hp = max_hp
+
+
 player_stand0 = pygame.image.load('images/Player/player_standing/img0.png')
 player_stand0 = pygame.transform.scale(player_stand0, (102, 102))
 player_stand1 = pygame.image.load('images/Player/player_standing/img1.png')
@@ -64,14 +137,71 @@ player_walk4 = pygame.transform.scale(player_walk4, (102, 102))
 
 def makePlayer(x, y):
     entity = engine.Entity()
-    entity.position = engine.Position(x,y, 60, 60)
+    entity.position = engine.Position(x,y, 60, 300)
     entityStandingAnimation = engine.Animation([player_stand0, player_stand1, player_stand2, player_stand3, player_stand4, player_stand5])
     entityWalkingAnimation = engine.Animation([player_walk0, player_walk1, player_walk2, player_walk3, player_walk4])
     entity.animations.add('standing', entityStandingAnimation)
     entity.animations.add('walking', entityWalkingAnimation)
+    # entity.score = engine.Score()
     entity.type = 'player'
     return entity
 
+player1_stand0 = pygame.image.load("images/Player1/Standing/img0.png")
+player1_stand0 = pygame.transform.scale(player1_stand0, (102, 102))
+player1_stand1 = pygame.image.load("images/Player1/Standing/img1.png")
+player1_stand1 = pygame.transform.scale(player1_stand1, (102, 102))
+player1_stand2 = pygame.image.load("images/Player1/Standing/img2.png")
+player1_stand2 = pygame.transform.scale(player1_stand2, (102, 102))
+player1_stand3 = pygame.image.load("images/Player1/Standing/img3.png")
+player1_stand3 = pygame.transform.scale(player1_stand3, (102, 102))
+player1_stand2 = pygame.image.load("images/Player1/Standing/img2.png")
+player1_stand2 = pygame.transform.scale(player1_stand2, (102, 102))
+player1_stand1 = pygame.image.load("images/Player1/Standing/img1.png")
+player1_stand1 = pygame.transform.scale(player1_stand1, (102, 102))
+def makePlayer1(x, y):
+    entity = engine.Entity()
+    entity.position = engine.Position(x,y, 60, 300)
+    entityStandingAnimation = engine.Animation([player1_stand0, player1_stand1, player1_stand2, player1_stand3, player1_stand2, player1_stand1])
+    # entityWalkingAnimation = engine.Animation([player_walk0, player_walk1, player_walk2, player_walk3, player_walk4])
+    entity.animations.add('standing', entityStandingAnimation)
+    # entity.animations.add('walking', entityWalkingAnimation)
+    # entity.score = engine.Score()
+    # entity.alive = True
+    entity.damage = 5
+    entity.current_fighter = 1
+    entity.type = 'player'
+    return entity
+
+
+enemy_stand0 = pygame.image.load('images/Enemy/Idle/0.png')
+enemy_stand0 = pygame.transform.scale(enemy_stand0, (200, 110))
+enemy_stand1 = pygame.image.load('images/Enemy/Idle/1.png')
+enemy_stand1 = pygame.transform.scale(enemy_stand1, (200, 110))
+enemy_stand2 = pygame.image.load('images/Enemy/Idle/2.png')
+enemy_stand2 = pygame.transform.scale(enemy_stand2, (200, 110))
+enemy_stand3 = pygame.image.load('images/Enemy/Idle/3.png')
+enemy_stand3 = pygame.transform.scale(enemy_stand3, (200, 110))
+enemy_stand4 = pygame.image.load('images/Enemy/Idle/4.png')
+enemy_stand4 = pygame.transform.scale(enemy_stand2, (200, 110))
+enemy_stand5 = pygame.image.load('images/Enemy/Idle/5.png')
+enemy_stand5 = pygame.transform.scale(enemy_stand5, (200, 110))
+enemy_stand6 = pygame.image.load('images/Enemy/Idle/6.png')
+enemy_stand6 = pygame.transform.scale(enemy_stand0, (200, 110))
+enemy_stand7 = pygame.image.load('images/Enemy/Idle/7.png')
+enemy_stand7 = pygame.transform.scale(enemy_stand0, (200, 110))
+
+def makeEnemy(x, y, damage, potions):
+    entity = engine.Entity()
+    entity.position = engine.Position(x,y, 60, 300)
+    entity.rect = pygame.rect.Rect(480, 340, 150, 150)
+    entityStandingAnimation = engine.Animation([enemy_stand0, enemy_stand1, enemy_stand2, enemy_stand3, enemy_stand4, enemy_stand5, enemy_stand6, enemy_stand7])
+    entity.animations.add('standing', entityStandingAnimation)
+    entity.damage = damage
+    entity.potions = potions
+    # entity.alive = True
+    entity.current_fighter = 2
+    entity.type = 'enemy'
+    return entity
 def makeBackground(name):
     window = pygame.display.set_mode((720, 480))
     window.blit(name, (0, 0))
@@ -97,6 +227,10 @@ def draw_coinText(window,text, x, y):
     # window = pygame.display.set_mode((720, 480))
     window.blit(text, text_rectangle)
 
+
+def draw_text1(window, text, font, color, x, y):
+    img = font.render(text, True, color)
+    window.blit(img, (x, y))
 
 
 #
@@ -147,20 +281,63 @@ def intro_text(window, text, size, x, y):
 
 
 
-def npcName(window):
+def npcName(window, text, color, x, y):
     # p_background = pygame.image.load('images/Prison/prison.png')
     # p_background = pygame.transform.scale(p_background, (GAME_WIDTH, GAME_HEIGHT))
     # window.blit(p_background, (0, 0))
     # coin_count_image = pygame.image.load('images/Coins/coin.png')
     # coin_count_image = pygame.transform.scale(coin_count_image, (25, 30))
     gui_font = pygame.font.SysFont('franklingothicmedium', 24)
-    text = "Spiculus"
-    name_surface = gui_font.render(text, False, '#FFFFFF')
-    window.blit(name_surface, (180, 215))
+    text = text
+    # color = color
+    name_surface = gui_font.render(text, False, color)
+    window.blit(name_surface, (x, y))
     # window.blit(coin_count_image, (50, 50))
     # coin_number = 0
     # draw_coinText(window, str(coin_number), 80, 52)
 
 
+gui_font = pygame.font.SysFont('franklingothicmedium', 24)
+font = pygame.font.SysFont('franklingothicmedium', 15)
+
+second_surface = pygame.Surface([720, 400])
+second_surface.fill((BLACK))
+door_text = "press 1 to go to hall"
+text0 = "press E to talk"
+text1 = "Hello, I am Spiculus. They brought you here last night. I don't know "
+text2 = "what crime you committed, but you got into the gladiatorial arena."
+text3 = "You have to defeat three chosen gladiators and then you will be free. "
 
 
+dialogue_surface0 = font.render(text0, False, '#FFFFFF')
+door_surface = font.render(door_text, False, '#FFFFFF')
+dialogue_surface1 = gui_font.render(text1, False, '#FFFFFF')
+dialogue_surface2 = gui_font.render(text2, False, '#FFFFFF')
+dialogue_surface3 = gui_font.render(text3, False, '#FFFFFF')
+
+turn1_text = "Your Turn"
+turn2_text = "Enemy's Turn"
+turn_surface1 = gui_font.render(turn1_text, False, '#FFFFFF')
+turn_surface2 = gui_font.render(turn2_text, False, '#FFFFFF')
+
+hall_text = "Press B to go back to the Hall"
+
+sword_text = "+ 3 DMG"
+sword_surface = font.render(sword_text, False, '#FFFFFF')
+armor_text = "+ 7 HP"
+armor_surface = font.render(armor_text, False, '#FFFFFF')
+potion_text = "HEALS"
+potion_surface = font.render(potion_text, False, '#FFFFFF')
+pcost_text = "COST: 10"
+pcost_surface = font.render(pcost_text, False, '#FFFFFF')
+acost_text = "COST: 20"
+acost_surface = font.render(acost_text, False, '#FFFFFF')
+scost_text = "COST: 20"
+scost_surface = font.render(scost_text, False, '#FFFFFF')
+
+cursor_image = pygame.image.load("images/ShopItems/sword.png")
+
+won_text = "You Won!"
+won_surface = gui_font.render(won_text, False, '#FFFFFF')
+lost_text = "You Lost!"
+lost_surface = gui_font.render(lost_text, False, '#FFFFFF')
